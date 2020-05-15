49c604395f9b91366bcb201d8a9ba39669459d72
Amit Talapatra <amit.talapatra@gmail.com>
2020-05-15 14:45:45 -0400

    Added ways to connect to and view collections

--- Python with MongoDB/Python with MongoDB.py
+++ Python with MongoDB/Python with MongoDB.py
@@ -3,29 +3,34 @@ from pymongo import MongoClient
 import pprint
 import json
 
-connections = json.load(open('C:/Users/amitt/OneDrive/Code/2020-05-08 Python with MongoDB/connection.json'))
-uri = connections['MongoDB Atlas']['mflix URI']
+if __name__ == "__main__":
+    # Identify connection information
+    connections = json.load(open('C:/Users/amitt/OneDrive/Code/2020-05-08 Python with MongoDB/connection.json'))
+    uri = connections['MongoDB Atlas']['mflix URI']
 
-client = MongoClient(uri)
+    # Connect to database
+    client = MongoClient(uri)
 
-db = client.aggregations
+    # View database info
+    client.server_info()
 
+    # View databases
+    client.database_names()
 
+    # Connect to a database and view collections
+    db = client.pubs
+    db.collection_names()
 
+    # Bring collections into Python
+    british_pubs = db['british_pubs']
+    welsh_pubs = db['welsh_pubs']
 
+    # View a document
+    pprint.pprint(british_pubs.find_one())
 
-connection_data = json.dumps(connections, indent=4)
+    # Save document in Python
+    document = british_pubs.find_one()
 
-client = MongoClient(uri)
-
-
-
-client.database_names()
-
-db = client.aggregations
-
-db.list_collection_names()
-
-air_alliances = db.air_alliances
-
-pprint.pprint(air_alliances.find_one())
+    # Print all docs in collection
+    for doc in british_pubs.find():
+        print(doc)
