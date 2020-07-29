5c3fe2242d59e317764acae6c4c3e282d320b444
Amit Talapatra <amit.talapatra@gmail.com>
2020-07-29 18:05:45 -0400

    Added API Key Code

--- Twitter API/Tweepy.py
+++ Twitter API/Tweepy.py
@@ -5,8 +5,27 @@ https://towardsdatascience.com/tweepy-for-beginners-24baf21f2c25
 """
 
 import tweepy
+import json
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+connections = json.load(open('C:/Users/amitt/OneDrive/Code/2020-07-29 Twitter API Keys/connection.json'))
+api_key = connections['Twitter API']['API Key']
+api_secret_key = connections['Twitter API']['API Secret Key']
+bearer_token = connections['Twitter API']['Bearer Token']
+access_token = connections['Twitter API']['Access Token']
+access_token_secret = connections['Twitter API']['Access Token Secret']
+
+
+
+
+
+
+
+
+
+
+
+
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
