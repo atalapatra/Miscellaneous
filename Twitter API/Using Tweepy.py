e5f89fbbab260273ef05eec4e40159d4ffb1fb94
Amit Talapatra <amit.talapatra@gmail.com>
2020-08-09 14:07:47 -0400

    buildTestSet function added

--- Twitter API/Using Tweepy.py
+++ Twitter API/Using Tweepy.py
@@ -3,11 +3,29 @@ Useful Resources:
 http://docs.tweepy.org/en/v3.5.0/getting_started.html 
 https://towardsdatascience.com/tweepy-for-beginners-24baf21f2c25
 http://docs.tweepy.org/en/latest/
+
+Creating The Twitter Sentiment Analysis Program in Python with Naive Bayes Classification
+https://towardsdatascience.com/creating-the-twitter-sentiment-analysis-program-in-python-with-naive-bayes-classification-672e5589a7ed
 """
 
 import tweepy
 import json
 import pandas as pd
+
+## Functions
+
+def buildTestSet(search_keyword):
+    try:
+        tweets_fetched = api.GetSearch(search_keyword, count = 100)
+        
+        print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
+        
+        return [{"text":status.text, "label":None} for status in tweets_fetched]
+    except:
+        print("Unfortunately, something went wrong..")
+        return None
+
+## Analysis
 
 connections = json.load(open('C:/Users/amitt/OneDrive/Code/2020-07-29 Twitter API Keys/connection.json'))
 api_key = connections['Twitter API']['API Key']
@@ -21,9 +39,17 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print (tweet.text)
+# # Test that Twitter API connection is working
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print (f"{tweet.user.name}: {tweet.text}")
+# 
+# public_tweets = pd.DataFrame(public_tweets)
+    
+search_term = input("Enter a search keyword:")
+testDataSet = buildTestSet(search_term)
 
-public_tweets = pd.DataFrame(public_tweets)
-public_tweets.iloc(1)
+print(testDataSet[0:4])
+
+tweets_fetched = api.search("game", count = 100)
+
